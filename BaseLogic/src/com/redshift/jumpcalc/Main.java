25366e76d89fdf249f50e17b601291d6494527d8
Dmitry T <tereshonok@protonmail.com>
2020-01-29 22:53:34 +0300

    missing files, gitignore

---
+++ BaseLogic/src/com/redshift/jumpcalc/Main.java
@@ -0,0 +1,12 @@
+package com.redshift.jumpcalc;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //Logic logic = new Logic();
+        LogicNew logic = new LogicNew();
+        Input.input();
+
+    }
+
+}
